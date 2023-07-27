afef1e4e7d9e4aa60786f361a7a026e72588b5de
Sharion2023 <Sharion06@gmail.com>
2023-07-27 12:49:52 -0500

    Added listeners

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Write your JavaScript code here!
 
-
-
 window.addEventListener("load", function() {
-    formSubmission()
-    validateInput()
-    console.log("Blah")
-    list = document.getElementById("faultyItems")
-    console.log(list)
-    giveAlert();
+
+    addEventListener("submit", function(event){
+        event.preventDefault()
+    })
+    
+    //giveAlert();
+    formSubmission();
+    
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -20,5 +20,15 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+
+   form.addEventListener("submit", function(event){
+    pilot = document.querySelector("input[name=pilotName]")
+    copilot = document.querySelector("input[name=copilotName]")
+    fuelLevel = document.querySelector("input[name=fuelLevel]")
+    cargoLevel = document.querySelector("input[name=cargoMass]")   
+    list = document.getElementById("faultyItems")
+    console.log(list)
+
+})
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,47 +2,66 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
-function giveAlert(){
+function giveAlert() {
     let form = document.querySelector("form");
-    form.addEventListener("submit", function(event){
+    form.addEventListener("submit", function (event) {
         alert("All fields are required")
     });
 }
 
 function validateInput(testInput) {
-    console.log("here")
-
-   if (isNaN(testInput)){
-    return "Not a Number";
-   } else if (!(isNaN(testInput))) {
-    return "Is a Number";
-   } else if (testInput === ""){
-    return "Empty";
-   }
+    if (isNaN(testInput)) {
+        return "Not a Number";
+    } else if (!(isNaN(testInput))) {
+        return "Is a Number";
+    } else if (testInput === "") {
+        return "Empty";
+    }
 }
 
-    
 
+//You'd access those from your HTML, and place them in a script in an eventlistener that is specific for submission, 
+//because you're looking to grab that information when they input it and hit the submit button
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    pilot = document.querySelector("input[name:pilotName]")
-    copilot = document.querySelector("input[name:copilotName]")
-    fuelLevel = document.querySelector("input[name:fuelLevel]")
-    cargoLevel = document.querySelector("input[name:cargoMass]")
-    list = document.getElementById("faultyItems")
+    console.log("here")
+    console.log(list)
+    
+    
+    
+    
+    strings = [pilot, copilot]
+    integers = [fuelLevel, cargoLevel]
+
+       //for(let i=0; i < strings.length; i++){
+            validateInput(pilot)
+            if(pilot !== "Not a Number"){
+                alert("Please enter a valid response!")
+            }
+     //   }
+        
+       /* for(let i=0; i < integers.length; i++){
+            validateInput(integers)
+            if(strings[i] !== "Is a Number"){
+                alert("Please enter a valid response!")
+            }
+        }*/
+
+    }
+    
     //console.log(list)
     /*function validateInput(pilot){
     console.log("here")
@@ -52,13 +71,12 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             alert("Please enter a valid response")
         }
    } */
-}
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -69,5 +87,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
